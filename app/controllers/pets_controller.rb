a65b63d0d8476233c46ade659bde162c9400a887
YosefRosenberg <yosef.e.rosenberg@gmail.com>
2016-03-25 11:06:14 -0700

    paperclip and user authorization

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -88,6 +88,6 @@ class PetsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pet_params
-      params.require(:pet).permit(:name, :breed, :size, :age, :zipcode)
+      params.require(:pet).permit(:name, :breed, :size, :age, :zipcode, :image)
     end
 end
